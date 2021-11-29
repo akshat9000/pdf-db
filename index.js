d24d17c046eac1accd54019e183696ddfca9365e
Akshat Srivastava <srivastava.akshat9000@gmail.com>
2021-11-29 21:55:36 +0530

    yolo

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const cors = require('cors')
 const firebase = require('./fbdb')
 
 app.use(express.json())
+
+
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors())     
 
 app.set('view engine','ejs')
 
